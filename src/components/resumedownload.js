8428dbc3b870ff3252a064cec79d60e538f8ae2f
anujrathor <41207048+anujrathor@users.noreply.github.com>
2020-04-21 02:33:18 +0530

    download button done

--- src/components/resumedownload.js
+++ src/components/resumedownload.js
@@ -1,13 +1,22 @@
 import React from "react";
+import axios from "axios";
 
 
-const Resume = ()=>{
+const Resume = () => {
+
+    const download = () => {
+        axios.get('https://portfolio-anuj.herokuapp.com/users/resume')
+    }
 
 
 
-    return(
+
+
+    return (
         <div>
-            <button className="btn btn-sm text-light shadow heartbeat" style={{background:"rgb(255, 74, 87)",fontSize:'14px'}}>Download Resume<i class="fa fa-download mx-2"></i></button>
+            <button className="btn btn-sm text-light shadow heartbeat"
+                style={{ background: "rgb(255, 74, 87)", fontSize: '14px' }}
+                onClick={()=>download()}>Download Resume<i class="fa fa-download mx-2"></i></button>
         </div>
     )
 }
